709ff418dac0b0eea793f935da89de0e6f8ee3d2
Fatih Nar <fenar@yahoo.com>
2021-06-11 17:08:44 -0500

    Update 01-deploy.sh

--- 01-deploy.sh
+++ 01-deploy.sh
@@ -4,6 +4,8 @@ echo -e "Creating AWS OCP Cluster Manifests\n"
 if [ ! -f config/install-config.yaml.bak ]; then
     echo "1st run!"
     openshift-install create install-config
+    echo -e "Set SDN from OpenShiftSDN to OVNKubernetes\n"
+    sed -i -e 's/OpenShiftSDN/OVNKubernetes/g' install-config.yaml
     cp install-config.yaml config/install-config.yaml.bak
 else
     echo "Re-run!"
