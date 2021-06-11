b55c3941c9c2f105c8be042640fc7edc6f2be3a3
Fatih Nar <fenar@yahoo.com>
2021-06-11 15:28:59 -0500

    Update 01-deploy.sh

--- 01-deploy.sh
+++ 01-deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #Author: fenar
 echo -e "Creating AWS OCP Cluster Manifests\n"
-cp config/install-config.yaml.bak install-config.yaml
+if [ ! -f /1strun.txt ]; then
+    echo "1st run!"
+    openshift-install create install-config
+    cp install-config.yaml config/install-config.yaml.bak
+    touch 1strun.txt
+else
+    cp config/install-config.yaml.bak install-config.yaml
+fi
 openshift-install create manifests
 echo -e "Copying Add-On OVN IPSec Manifests\n"
 cp config/cluster-network-03-config.yml manifests/cluster-network-03-config.yml
