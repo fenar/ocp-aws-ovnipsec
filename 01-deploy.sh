2b90b5caa8586d4e057309d5aefb347fbe008900
Fatih Nar <fenar@yahoo.com>
2021-06-11 15:31:22 -0500

    Update 01-deploy.sh

--- 01-deploy.sh
+++ 01-deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #Author: fenar
 echo -e "Creating AWS OCP Cluster Manifests\n"
-if [ ! -f 1strun.txt ]; then
+if [ ! -f config/1strun.txt ]; then
     echo "1st run!"
     openshift-install create install-config
     cp install-config.yaml config/install-config.yaml.bak
-    touch 1strun.txt
+    touch config/1strun.txt
 else
+    echo "Re-run!"
     cp config/install-config.yaml.bak install-config.yaml
 fi
 openshift-install create manifests
